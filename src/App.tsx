6a71726b6d2f0d5c7b4acf109a9339c2d02a7b34
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2026-02-19 12:33:38 -0600

    +`raw-svg.tsx`
    Other approaches were too restrictive. This new approach combines the best of editing raw SVG with the best of react for dynamic, fine-tuned control (i.e., glyphs can be redrawn with precision given any stroke width).

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import {Braille} from './braille'
 import {calcRuneCoords, Metal, STROKE_WIDTH} from './norse-runes/'
 import {calcRuneCoords2} from './norse-runes/calc-rune-coords2'
 import {Metal2} from './norse-runes/metal2'
+import {SVGRunes} from './norse-runes/raw-svg'
 
 function App() {
 	return (
 		<>
 			<h1>Runic English (Metal)</h1>
 			<Metal
+			<SVGRunes />
 			<Metal2
 				config={{
 					glyphCoords: calcRuneCoords2(STROKE_WIDTH),
