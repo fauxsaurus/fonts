fe18e8811d3ab65fce9e363cfe7986ce8d9188bc
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2026-02-25 16:13:25 -0600

    +`gt()`
    To replace `bTriangle`.

--- src/norse-runes/stroke-components.ts
+++ src/norse-runes/stroke-components.ts
@@ -1,4 +1,4 @@
-import {mirrorPtsV, type IPts} from './util'
+import {lines2intersectionPt, mirrorPtsV, pt, type IPts} from './util'
 
 /** @note LINES
  * ASCENDER LINE (top, Cap Height)
@@ -6,6 +6,30 @@ import {mirrorPtsV, type IPts} from './util'
  * BASELINE (un-DESCENDed lowercase bottom)
  * DESCENDER LINE
  */
+
+/** @note ">" shape */
+export const gt = (sw: number): IPts => {
+	const sw2 = sw / 2
+	const swD45 = (sw * 2) / Math.SQRT2 // diagonal stroke width (@ 45 deg angle)
+
+	const topOuter = pt(0, 1024 - sw2)
+	const topInner = pt(0, 1024 - sw2 + swD45)
+	// @note the point where two 45 degree angle strokes meet is thicker--hence swD45
+	const bottomInner = pt(0, 2048 - swD45)
+	const bottomOuter = pt(0, 2048)
+
+	const centerInner = lines2intersectionPt(topInner, 1, bottomInner, -1)
+	const centerOuter = lines2intersectionPt(bottomOuter, -1, topOuter, 1)
+
+	return [
+		topOuter,
+		centerOuter,
+		bottomOuter,
+		bottomInner,
+		centerInner,
+		topInner,
+	]
+}
 
 export const tip = (sw: number): IPts => {
 	const sw2 = sw / 2
