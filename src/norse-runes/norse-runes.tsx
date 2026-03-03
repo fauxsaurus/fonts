7b74ed2f53dd46adb077f700d89261c079e5fb74
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2026-03-03 12:58:39 -0600

    shortened `GlyphStrokes`
    (For improved readability.)

--- src/norse-runes/norse-runes.tsx
+++ src/norse-runes/norse-runes.tsx
@@ -691,29 +691,13 @@ const d = (sw: number): IPts[] => {
 }
 
 const GlyphStrokes = {
-	B,
-	C,
-	E,
-	G,
-	N,
-	P,
-	a,
-	b,
-	c,
-	d,
-	e,
-	f,
-	h,
-	i,
-	k,
-	l,
-	m,
-	n,
-	o,
-	p,
-	r,
-	s,
-	t,
+	...{B, C, E, G, N, P},
+	...{a, b, c, d, e, f},
+	// g
+	...{h, i, k, l, m, n, o, p},
+	//q
+	...{r, s, t},
+
 	':': (sw: number): IPts[] => {
 		const topDotUpperTip = translatePts([0, 1024 - sw * 3], tipN)
 		const topDotLowerTip = translatePts(
