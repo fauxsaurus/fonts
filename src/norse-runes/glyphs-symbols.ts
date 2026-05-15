67d4258cfa46fbadf95194756f18814ba8b1097b
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2026-05-14 19:22:48 -0500

    +multi-face colon

--- src/norse-runes/glyphs-symbols.ts
+++ src/norse-runes/glyphs-symbols.ts
@@ -1,9 +1,22 @@
-import {vertical} from './stroke-components'
-import {mirrorPtsV, type IPts} from './util'
+import {i, o} from './glyphs-lowercase'
+import {mirrorPtsV, pts2MaxY, pts2MinY, translatePtsY, type IPts} from './util'
 
 export const colon = (sw: number): IPts[] => {
-	const sw2 = sw / 2 // offset width
-	const dot = vertical(sw, sw * 2, 1024 - sw2 * 2)
+	const minY = pts2MinY(o(sw).flat())
+	const midY = (2048 - minY) / 2 + minY
 
-	return [dot, mirrorPtsV(1024, dot)]
+	// get faces above the i's stem
+	const dotFaces = i(sw).filter((face) => pts2MaxY(face) < minY)
+
+	// amount to move dot down to be in line with lower case letters
+	const adjValue = minY - pts2MinY(dotFaces.flat())
+
+	const loweredDotFaces = dotFaces.map((face) =>
+		translatePtsY(adjValue, face)
+	)
+
+	return [
+		...loweredDotFaces,
+		...loweredDotFaces.map((face) => mirrorPtsV(midY, face)),
+	]
 }
