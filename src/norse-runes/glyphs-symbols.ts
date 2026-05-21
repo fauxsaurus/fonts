4539a9ab945aca018f0f6f490fa2eda252e29dcb
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2026-05-21 12:33:30 -0500

    +: outlines

--- src/norse-runes/glyphs-symbols.ts
+++ src/norse-runes/glyphs-symbols.ts
@@ -1,21 +1,22 @@
-import {i, o} from './glyphs-lowercase'
-import {mirrorPtsV, pts2MaxY, pts2MinY, translatePtsY, type IPts} from './util'
+import {o} from './glyphs-lowercase'
+import {dot} from './stroke-components'
+import {mirrorPtsV, pts2MinY, translatePtsY, type IPts} from './util'
 
 export const colon = (sw: number): IPts[] => {
+	const {outline, faces} = dot(sw) // positioned at the height it appears in i
+
 	const minY = pts2MinY(o(sw).flat())
 	const midY = (2048 - minY) / 2 + minY
 
-	// get faces above the i's stem
-	const dotFaces = i(sw).filter((face) => pts2MaxY(face) < minY)
-
 	// amount to move dot down to be in line with lower case letters
-	const adjValue = minY - pts2MinY(dotFaces.flat())
+	const adjValue = minY - pts2MinY(faces.flat())
 
-	const loweredDotFaces = dotFaces.map((face) =>
-		translatePtsY(adjValue, face)
-	)
+	const loweredDotFaces = faces.map((face) => translatePtsY(adjValue, face))
 
 	return [
+		translatePtsY(adjValue, outline),
+		mirrorPtsV(midY, translatePtsY(adjValue, outline)),
+
 		...loweredDotFaces,
 		...loweredDotFaces.map((face) => mirrorPtsV(midY, face)),
 	]
