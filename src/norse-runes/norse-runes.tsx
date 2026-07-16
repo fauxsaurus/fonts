a1768da78e892569f8e9ac54115104bef968dba1
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2026-07-15 21:44:39 -0500

    -dead code

--- src/norse-runes/norse-runes.tsx
+++ src/norse-runes/norse-runes.tsx
@@ -117,12 +117,6 @@ const Line = (props: IProps) => {
 
 	const width = sum(glyphWidths) + sum(glyphGaps)
 
-	/** Adjusts the height of the svg to maintain a base line of `fontSize`px tall (by elongating the height if there were characters below the ) */
-	const proportionalHeight = (height / 2048) * props.fontSize
-
-	// maintain svg aspect ratio
-	const proportionalWidth = (width * props.fontSize) / 2048
-
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
